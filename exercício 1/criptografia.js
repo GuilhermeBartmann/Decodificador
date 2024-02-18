95ec49e2db7b089a02fcb908b2fd4b7d0b9dabef
GuilhermeBartmann <157774732+GuilhermeBartmann@users.noreply.github.com>
2024-02-17 23:00:37 -0300

    Update criptografia.js

--- exercício 1/criptografia.js
+++ exercício 1/criptografia.js
@@ -1,3 +1,8 @@
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+}
+ 
  function criptografarTexto() {
     const textoEntrada = document.getElementById('textoEntrada').value;
     let textoSaidaCriptografado = '';
